eec645f54e5bf870be28ea55a7767f503351a81d
raj nishad <rajnishad24082003@gmail.com>
2022-10-05 01:08:00 +0530

    commit of 1 am

---
+++ backend/schema/organisers.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const userData = new mongoose.Schema({
+    googleid:{
+        type: String,
+        required:true
+    },
+    displayname:{
+        type:String,
+        required:true
+    },
+    firstname:{
+        type:String,
+        required:true
+    },
+    googleEmail:{
+        type:String,
+        required:true
+    },
+    userimage:{
+        type:String
+    },
+    admin: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required:true
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model("Organiser",userData);
